148dcf97d2c734941814fee1587f5f64d787ab63
J0EK3R <J0EK3R@users.noreply.github.com>
2020-09-22 07:15:20 +0200

    update

--- prepare_update.pl
+++ prepare_update.pl
@@ -4,7 +4,7 @@ use File::Basename;
 use POSIX qw(strftime);
 use strict;
 
-my @filenames = ( "73_RainbirdController.pm",
+my @filenames = ( "73_RainbirdControler.pm",
 		  "74_RainbirdZone.pm");
 
 my $prefix = "FHEM";
